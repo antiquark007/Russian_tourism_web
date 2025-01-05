2b0e0f1f27c8f68257674582d5b699d8813e08f0
antiquark <141497727+antiquark007@users.noreply.github.com>
2025-01-05 22:10:59 +0530

    new addition

--- history/history.js
+++ history/history.js
@@ -1,31 +1,25 @@
 function initSlideshow(slideshowContainer) {
-    const slides = slideshowContainer.getElementsByClassName("slideshow-img");
+    const slides = slideshowContainer.getElementsByClassName("slide");
     let slideIndex = 0;
 
-
     function showSlides() {
-        // Hide all slides
+        // Hide all slides and text
         for (let i = 0; i < slides.length; i++) {
             slides[i].classList.remove("active");
         }
 
-
         slideIndex++;
         if (slideIndex >= slides.length) {
-            slideIndex = 0; // Reset to the first slide
+            slideIndex = 0;
         }
 
-
         slides[slideIndex].classList.add("active");
-
 
         setTimeout(showSlides, 3000);
     }
 
-
     showSlides();
 }
-
 
 document.addEventListener("DOMContentLoaded", () => {
     const slideshowContainers = document.getElementsByClassName("slideshow-container");
